1f308fd2159a8bcd25871aeda7fc337db26b80c9
tanglero4 <tiffanyanglero94@hotmail.com>
2023-01-13 20:04:59 -0500

    Added notes

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,8 @@ fetch(
   .then(function (response) {
     return response.json();
   })
-  .then(function (data) {
-    // for (i = 0; i < 5; i+6) {
-    //
-    // }
-  });
+  .then(function (data) {});
+// Search city function plugs in user input to api url
 function searchCity(cityName) {
   fetch(
     `https://api.openweathermap.org/geo/1.0/direct?appid=1e85ad1f16666a315c273aa6eb03df77&q=${cityName}`
@@ -24,66 +21,63 @@ function searchCity(cityName) {
     })
     .then(function (data) {
       findWeather(data[0].lat, data[0].lon);
-      // for (i = 0; i < 5; i+6) {
-      //
       console.log(data);
-      // }
     });
 }
+// Collects city input from user
 var searchBtn = document.getElementById("searchBtn");
 searchBtn.addEventListener("click", () => {
   var cityName = document.getElementById("city-input").value;
   searchCity(cityName);
 });
-
+// Function to find and display weather information
 function findWeather(lat, lon) {
   console.log(lat, lon);
+  // Get locations coordinates
   fetch(
     `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=1e85ad1f16666a315c273aa6eb03df77&units=imperial`
   )
     .then(function (response) {
       return response.json();
     })
+    // Collects and displays wether information from the API and attaches it to the HTML
     .then(function (weather) {
       console.log(weather);
       let city = weather.city.name;
-      console.log(city);
-      document.querySelector(".city-name").textContent = city;
-      // city = document.querySelector(".city-name").append(city);
-      document.querySelector(".current-date").append(new Date().toDateString());
-      //getting the Icon and make an image with it..
       let iconImage = weather.list[0].weather[0].icon;
-      console.log(iconImage);
-      //   document.querySelector(".icon").append(iconImage);
-      // let weatherIcon = document.createElement("img");
-
       let temperature = weather.list[0].main.temp;
-      document.querySelector(".temperature").append(temperature);
-
       let wind = weather.list[0].wind.speed;
-      document.querySelector(".wind").append(wind);
-
       let humidity = weather.list[0].main.humidity;
-      document.querySelector(".humidity").append(humidity);
-      console.log(weather);
       var iconPicture = document.querySelector(".icon");
       iconPicture.setAttribute(
         "src",
         `https://openweathermap.org/img/wn/${iconImage}@2x.png`
       );
-
+      document.querySelector(".city-name").textContent = city;
+      // Add Current date
+      document.querySelector(".current-date").append(new Date().toDateString());
+      // Adds temperature to the page
+      document.querySelector(".temperature").append(temperature);
+      // Adds wind speed to the page
+      document.querySelector(".wind").append(wind);
+      // Adds humidity level to the page
+      document.querySelector(".humidity").append(humidity);
+      // Displays daily forecast
       forecast(weather.list);
     });
 }
+// Loop through the 5 day forecast
 function forecast(weather) {
   console.log(weather);
+  // Loops through daily weather
   for (i = 0; i < weather.length; i += 8) {
     console.log(weather[i]);
     var div = document.createElement("div");
     div.className = "col-2 border border-info rounded";
+    // Adds daily cards and weather information
     var weatherCard = ` 
                 <h3 class="first-day p-4 placeholder-wave"></h3>
-                <section class="icon"></section>
+                <section class="icon"> </section>
                 <p>Temp: ${weather[i].main.temp}°F</p>
                 <p>Wind: ${weather[i].wind.speed}MPH</p>
                 <p>Humidity: ${weather[i].main.humidity}%</p>
